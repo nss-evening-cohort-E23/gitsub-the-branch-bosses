02df8b8aa4a6f4f98e381b3c0e23fb9b60066cdd
Michael Nunan <michael.nunan@outlook.com>
2023-05-13 11:18:39 -0500

    still writing reposondom function

--- main.js
+++ main.js
@@ -192,26 +192,94 @@ const renderToDom = (divId, htmlToRender) => {
 
  };
 
- const repoTagsOnDom = () => {
-  let repoTagDomString = "";
+ const repoInfoOnDom = () => {
+  const repoTagsOnDom = () => {
+    let repoTagDomString = "";
 
-  for (let repo of repoData) {
-    for (let tag of repo.tags) {
+    for (let repo of repoData) {
+      for (let tag of repo.tags) {
+        
+        if (tag !== undefined) {
+        repoTagDomString += `
+        <a href="" class="repo-tag">${tag}</a>
+        `
+        };
       
-      if (tag !== 0 || undefined) {
-      repoTagDomString += `
-      <a href="" class="repo-tag">${tag}</a>
-      `
       };
-     
-    };
-    renderToDom(`#${repo.name}-tags`, repoTagDomString);
-  }
+      renderToDom(`#${repo.name}-tags`, repoTagDomString);
+    }
+  };
+
+  const repoLangOnDom = () => {
+    let repoLangDomString = "";
+
+    for (let repo of repoData) {
+      for (let lang of repo.languages) {
+        
+        if (lang !== undefined) {
+        repoLangDomString += `
+        <div class="repo-lang">${lang}</div>
+        `
+        };
+      
+      };
+      renderToDom(`#${repo.name}-languages`, repoLangDomString);
+    }
+  };
+
+  const repoStarsOnDom = () => {
+    let repoStarsDomString = "";
+
+    for (let repo of repoData) {
+      for (let count of repo.stars) {
+        
+        if (count !== 0) {
+        repoStarsDomString += `
+        <div class="repo-stars"><path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Zm0 2.445L6.615 5.5a.75.75 0 0 1-.564.41l-3.097.45 2.24 2.184a.75.75 0 0 1 .216.664l-.528 3.084 2.769-1.456a.75.75 0 0 1 .698 0l2.77 1.456-.53-3.084a.75.75 0 0 1 .216-.664l2.24-2.183-3.096-.45a.75.75 0 0 1-.564-.41L8 2.694Z"></path>${count}</div>
+        `
+        };
+      
+      };
+      renderToDom(`#${repo.name}-stars`, repoStarsDomString);
+    }
+  };
+
+  const repoForksOnDom = () => {
+    let repoForksDomString = "";
+
+    for (let repo of repoData) {
+      for (let count of repo.forks) {
+        
+        if (count !== 0) {
+        repoForksDomString += `
+        <div class="repo-forks"><path d="M5 5.372v.878c0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75v-.878a2.25 2.25 0 1 1 1.5 0v.878a2.25 2.25 0 0 1-2.25 2.25h-1.5v2.128a2.251 2.251 0 1 1-1.5 0V8.5h-1.5A2.25 2.25 0 0 1 3.5 6.25v-.878a2.25 2.25 0 1 1 1.5 0ZM5 3.25a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Zm6.75.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm-3 8.75a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Z"></path>${count}</div>
+        `
+        };
+      
+      };
+      renderToDom(`#${repo.name}-forks`, repoForksDomString);
+    }
+  };
+
+  const repoLicenseOnDom = () => {
+    let repoLicenseDomString = "";
+
+    for (let repo of repoData) {
+      for (let license of repo.license) {
+        
+        if (license !== undefined) {
+        repoLicenseDomString += `
+        <div class="repo-license">${license}</div>
+        `
+        };
+      
+      };
+      renderToDom(`#${repo.name}-license`, repoLicenseDomString);
+    }
+  };
+  
+
  };
-
- 
-
-
 //  const repoInfoOnDom = () => {
 
 //   let repoInfoDomString = "";
